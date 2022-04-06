0ffe6a26a4874a6c86b2ccc6ad35efec6205c6e7
DanieLuan <danielluanlourencol@gmail.com>
2022-04-06 19:56:07 -0300

    soma vizinhos testes ok

--- soma_vizinhos/src/main.cpp
+++ soma_vizinhos/src/main.cpp
@@ -8,9 +8,27 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-int main( void )
-{
-    // TODO: Adicione seu código aqui. 
+int main( void ){
+    int n, m;
+    int sum = 0;
 
+    while(cin >> std::ws >> m >> n){
+        if(n > 0){
+            for(int i = 0; i < n; i++){
+                sum += m + i;
+            }
+        }
+        else if(n < 0){
+            for(int i = 0; i > n; i--){
+                sum += m + i;
+            }
+        }
+        else{
+            sum = m;
+        }
+
+        cout << sum << endl;
+        sum = 0;
+    }
     return 0;
 }
