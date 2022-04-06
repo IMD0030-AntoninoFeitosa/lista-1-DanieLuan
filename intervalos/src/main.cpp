88fa27de0a5c056c61b617b22b4381df5cb6ad93
DanieLuan <danielluanlourencol@gmail.com>
2022-04-06 01:43:48 -0300

    intervalos testes ok

--- intervalos/src/main.cpp
+++ intervalos/src/main.cpp
@@ -13,9 +13,41 @@ using std::setprecision;
 
 // Se desejar, crie funções aqui, antes do main().
 
-int main(void)
-{
-    // TODO: Adicione aqui a sua solução.
+int main(void){
+
+    int x;
+    double range[5];
+
+    range[0] = 0;
+    range[1] = 0;
+    range[2] = 0;
+    range[3] = 0;
+    range[4] = 0;
+
+    while(cin >> std::ws >> x){
+        if(x >= 0 && x < 25){ //Ver isso depois!
+            range[0]++;
+        }
+        else if(x >= 25 && x < 50){
+            range[1]++;
+        }
+        else if(x >= 50 && x < 75){
+            range[2]++;
+        }
+        else if(x >= 75 && x < 100){
+            range[3]++;
+        }
+        else{
+            range[4]++;
+        }
+    }
+
+    double total = range[0] + range[1] + range[2] + range[3] + range[4];
+
+    for(int i = 0; i < 5; i++){
+        cout << std::setprecision(4) << (range[i]/total)*100.0 << std::endl;
+    }
+
 
     return 0;
 }
