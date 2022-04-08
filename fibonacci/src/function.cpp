8685c9a88b4b84be6a33f5f2d0fd9da3fd859be3
Unknown <daniel.lourenco.711@A304-17L.imd.local>
2022-04-07 22:00:32 -0300

    fibonacci start

--- fibonacci/src/function.cpp
+++ fibonacci/src/function.cpp
@@ -5,5 +5,24 @@ std::vector<unsigned int> fib_below_n( unsigned int n )
     // TODO: adicione o seu código aqui.
 
     // TODO: Isto é apenas um STUB. Troque o retorno pelo que você julgar correto.
-    return std::vector<unsigned int>{};
+    std::vector<unsigned int> fibs;
+    unsigned int fib1 = 0;
+    unsigned int fib2 = 1;
+    unsigned int fib3 = 0;
+
+    fibs.push_back(fib2);
+
+    while(fib3 < n){
+
+        fib3 = fib1 + fib2;
+        fib1 = fib2;
+        fib2 = fib3;
+
+        if(fib3 > n){
+            break;
+        }
+        fibs.push_back(fib3);
+    }
+
+    return fibs;
 }
