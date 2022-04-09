defb7e014fabe005465290507fc2116f98290f4e
DanieLuan <danielluanlourencol@gmail.com>
2022-04-09 16:58:11 -0300

    inverter testes ok + gitignore /builds

--- inverter/src/function.cpp
+++ inverter/src/function.cpp
@@ -6,7 +6,17 @@
  * @param arr Reference to the array with the values.
  */
 template <std::size_t SIZE>
-void reverse( std::array< std::string, SIZE > & arr )
-{
-    /*codigo*/
+void reverse( std::array< std::string, SIZE > & arr ){
+    int end = arr.size() - 1;
+    int start = 0;
+    std::string aux;
+
+    while(start < end){
+        aux = arr[start];
+        arr[start] = arr[end];
+        arr[end] = aux;
+
+        start++;
+        end--;
+    }
 }
